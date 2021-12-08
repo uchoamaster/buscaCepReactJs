270ab7bf2039f8ab8a093b4ac0484b0c704c4e5b
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-08 15:34:12 -0300

    criando conexao com api e iniciando a criação da logica

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    // alert("Valor do Input " + input)
+    //https://viacep.com.br/ws/01310930/json/
+
+    if(input === ''){
+      alert('Preencha algum CEP!')
+      return;
+    }
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+
   return (
     <div className="container">
      <h1 className="title">Buscador CEP</h1>
@@ -8,9 +28,11 @@ function App() {
        <input
        type="text"
        placeholder="Digite seu cep ..."
+       value={input}
+       onChange={(event) => setInput(event.target.value)}
        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
             <FiSearch size={25} color="#fff"/>
         </button>
      </div>
